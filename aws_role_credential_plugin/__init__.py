54851251bc9d8260ca1a91ff8a9ba0032c04ea02
Derek Waters <derek@frisbeeworld.com>
2023-12-15 22:07:52 +1100

    Code quality improvements

--- aws_role_credential_plugin/__init__.py
+++ aws_role_credential_plugin/__init__.py
@@ -39,10 +39,6 @@ def aws_role_credential_backend(**kwargs):
 
         credentials = response.get("Credentials", {})
 
-        #new_connection = boto3.Session(aws_access_key_id=credentials['AccessKeyId'],
-        #              aws_secret_access_key=credentials['SecretAccessKey'],
-        #              aws_session_token=credentials['SessionToken'])
-
         _aws_cred_cache[role_arn] = credentials
 
     credentials = _aws_cred_cache.get(role_arn, None)
@@ -56,21 +52,7 @@ aws_role_credential_plugin = CredentialPlugin(
     'AWS Role Credential Plugin',
     # see: https://docs.ansible.com/ansible-tower/latest/html/userguide/credential_types.html
     # inputs will be used to create a new CredentialType() instance
-    #
-    # inputs.fields represents fields the user will specify *when they create*
-    # a credential of this type; they generally represent fields
-    # used for authentication (URL to the credential management system, any
-    # fields necessary for authentication, such as an OAuth2.0 token, or
-    # a username and password). They're the types of values you set up _once_
-    # in AWX
-    #
-    # inputs.metadata represents values the user will specify *every time
-    # they link two credentials together*
-    # this is generally _pathing_ information about _where_ in the external
-    # management system you can find the value you care about i.e.,
-    #
-    # "I would like Machine Credential A to retrieve its username using
-    # Credential-O-Matic B at identifier=some_key"
+    # see: https://github.com/ansible/awx-custom-credential-plugin-example
     inputs={
         'fields': [{
             'id': 'access_key',
@@ -79,7 +61,7 @@ aws_role_credential_plugin = CredentialPlugin(
         }, {
             'id': 'secret_key',
             'label': 'AWS Secret Key',
-            'type': 'string',
+            'type': 'password',
         }, {
             'id': 'aws_region',
             'label': 'AWS Default Region',
@@ -93,15 +75,9 @@ aws_role_credential_plugin = CredentialPlugin(
             'id': 'identifier',
             'label': 'Identifier',
             'type': 'string',
-            'help_text': 'The name of the key in My Credential System to fetch.'
+            'help_text': 'The name of the key in the assumed AWS role to fetch [AccessKeyId | SecretAccessKey | SessionToken].'
         }],
         'required': ['access_key', 'secret_key', 'role_arn', 'aws_region'],
     },
-
-    # backend is a callable function which will be passed all of the values
-    # defined in `inputs`; this function is responsible for taking the arguments,
-    # interacting with the third party credential management system in question
-    # using Python code, and returning the value from the third party
-    # credential management system
     backend = aws_role_credential_backend
 )
